4ea01f633a4dc8b9720c6eaa6d20614cbc38a3b1
mmanickath <99034865+mmanickath@users.noreply.github.com>
2024-08-21 04:38:56 +0000

    I love this new technology

---
+++ madhav/final_project/emotion.detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = response.post(URL, json = input_json, headers=header)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        return formatted_response
+    elif response.status_code == 400:
+        formatted_response = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant emotion': None
+        }
+        return formatted_response
+
+ 
